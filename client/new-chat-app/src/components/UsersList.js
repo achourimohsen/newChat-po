f8f4e22b640bc42605abe86d8859696306b6a762
mohamed123 <chaabenim318@gmail.com>
2023-11-24 20:56:24 +0100

    update file

--- client/new-chat-app/src/components/UsersList.js
+++ client/new-chat-app/src/components/UsersList.js
@@ -111,31 +111,3 @@ export default function AlignItemsList() {
         </List>
     );
 }
-
-//  <ListItem alignItems="flex-start">
-//             <ListItemAvatar>
-//                 <Avatar
-//                     alt="Remy Sharp"
-//                     src="/static/images/avatar/1.jpg"
-//                 />
-//             </ListItemAvatar>
-//             <ListItemText
-//                 primary="Brunch this weekend?"
-//                 secondary={
-//                     <React.Fragment>
-//                         <Typography
-//                             sx={{ display: "inline" }}
-//                             component="span"
-//                             variant="body2"
-//                             color="text.primary"
-//                         >
-//                             Ali Connors
-//                         </Typography>
-//                         {
-//                             " — I'll be in your neighborhood doing errands this…"
-//                         }
-//                     </React.Fragment>
-//                 }
-//             />
-//         </ListItem>
-//         <Divider variant="inset" component="li" />
